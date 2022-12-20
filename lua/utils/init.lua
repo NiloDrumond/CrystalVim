a3ea35bf0409a82252871bd6190a621fab5a5e0e
Nilo Drumond <nilobdrumond@gmail.com>
2022-12-20 15:05:21 -0300

    feat(util): add spread function

--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -4,4 +4,40 @@ M.add_whitespaces = function(number)
   return string.rep(" ", number)
 end
 
+
+local function deep_copy(object)
+  if type(object) ~= 'table' then return object end
+
+  local result = {}
+  for key, value in pairs(object) do
+    result[key] = deep_copy(value)
+  end
+  return result
+end
+
+local function spread(template, override)
+  if not override then
+    return function(override)
+      spread(template, override)
+    end
+  end
+
+  local mt = getmetatable(template)
+  local result = {}
+  setmetatable(result, mt)
+
+  for key, value in pairs(template) do
+    result[key] = deep_copy(value)
+  end
+
+  -- No longer wrapped up inside a function
+  for key, value in pairs(override) do
+    result[key] = value
+  end
+
+  return result
+end
+
+M.spread = spread
+
 return M
